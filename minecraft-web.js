c188aa4fe9e3caa46004cc43214111dbd3069ad7
Alessandro Pignotti <a.pignotti@sssup.it>
2025-02-28 12:12:31 +0100

    Restructure code to remove the use of shadow DOM

--- minecraft-web.js
+++ minecraft-web.js
@@ -34,88 +34,7 @@ async function downloadFileToCheerpJ(url, destPath, progressCallback) {
 	});
 }
 
-const template = document.createElement('template');
-template.innerHTML = `
-  <style>
-    :host {
-      display: inline-block;
-      aspect-ratio: 854 / 480;
-
-      background: black;
-      color: #eee;
-      color-scheme: dark;
-
-      width: 854px;
-      height: 480px;
-    }
-
-    :host([hidden]) {
-      display: none;
-    }
-
-    canvas {
-      width: inherit;
-      height: inherit;
-    }
-
-    .display {
-      width: 854px;
-      height: 480px;
-      position: absolute;
-      inset: 0;
-      visibility: hidden;
-    }
-
-    .intro {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      height: 100%;
-    }
-
-    p {
-      max-width: 60ch;
-    }
-
-    .disclaimer {
-      font-size: 0.8em;
-      opacity: 0.5;
-    }
-
-    button {
-      padding: 0.5em 1em;
-      margin: 2em;
-    }
-
-    progress {
-      width: calc(100% - 2em);
-      margin: 1em;
-    }
-
-    *:focus {
-      outline: none;
-    }
-  </style>
-  <canvas width="854" height="480" tabindex="-1"></canvas>
-  <div class="display"></div>
-  <div class="intro">
-    <p>
-      This is a proof-of-concept demo of Minecraft 1.2.5 running unmodified in the browser.
-    </p>
-    <p>
-      Clicking the button below will download the client from mojang.com.
-      By clicking it, you agree to the <a href="https://www.minecraft.net/eula">Minecraft EULA</a>.
-    </p>
-    <button>Play!</button>
-    <div class="disclaimer">
-      This is not an official Minecraft product. It is not approved by or associated with Mojang or Microsoft.
-    </div>
-  </div>
-  <progress style="display: none"></progress>
-`;
-
-export default class MinecraftClient extends HTMLElement {
+export default class MinecraftClient {
   #canvas;
   #progress;
   #button;
@@ -124,35 +43,25 @@ export default class MinecraftClient extends HTMLElement {
   #isRunning;
 
   constructor() {
-    super();
-
-    const shadowRoot = this.attachShadow({ mode: 'open' });
-    shadowRoot.appendChild(template.content.cloneNode(true));
-
-    this.#button = shadowRoot.querySelector('button');
+    this.#button = document.querySelector('button');
     this.#button.addEventListener('click', () => this.run());
 
-    this.#canvas = shadowRoot.querySelector('canvas');
+    this.#canvas = document.querySelector('canvas');
     this.#canvas.width = 854;
     this.#canvas.height = 480;
     this.#canvas.tabIndex = -1;
     this.#canvas.style.display = 'none';
-
-    this.#progress = shadowRoot.querySelector('progress');
+    this.#progress = document.querySelector('progress');
     this.#progress.style.display = 'none';
 
-    this.#intro = shadowRoot.querySelector('.intro');
+    this.#intro = document.querySelector('.intro');
 
-    // CheerpJ needs an element to render to, but we are going to render to own canvas
-    this.#display = shadowRoot.querySelector('.display');
+    // CheerpJ needs an element to render to
+    this.#display = document.querySelector('.display');
     this.#display.setAttribute('style', 'width:100%;height:100%;position:absolute;top:0;left:0px;visibility:hidden;');
     cheerpjCreateDisplay(-1, -1, this.#display);
 
     this.#isRunning = false;
-  }
-
-  static register() {
-    customElements.define('minecraft-client', this);
   }
 
   /** @returns {Promise<number>} Exit code */
