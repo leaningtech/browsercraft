9dc8b162728661581662ff252f67d65405fce8c1
Francesco <francesco@leaningtech.com>
2025-06-12 16:12:30 +0200

    done porting on svelteKit, cheerpJ APis do not work

---
+++ src/lib/cheerpj.d.ts
@@ -0,0 +1,12 @@
+declare const cheerpjInit: (options?: object) => Promise<void>;
+declare const cheerpjRunMain: any;
+declare const cheerpjRunJar: any;
+declare const cheerpjRunLibrary: any;
+declare const cheerpjCreateDisplay: any;
+declare const cjFileBlob: any;
+declare const cjGetRuntimeResources: any;
+declare const cjGetProguardConfiguration: any;
+declare const dumpMethod: any;
+declare const dumpClass: any;
+declare const dumpAllThreads: any;
+declare const cheerpjAddStringFile: any;
