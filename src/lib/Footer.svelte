84f9b8fe7557ecd0624b4b92a269ebea06659d19
Francesco <francesco@leaningtech.com>
2025-06-12 23:49:51 +0200

    added footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,12 @@
+<footer id="footer">
+	<hr class="solid">
+	<div id="footerLogo">
+		<img id = "ltlogo" src = "tower.svg" alt="Leaning Technologies logo"/>
+		<a href="https://leaningtech.com/" target=”_blank”>
+			<p id="footerText">© 2023-2025 Leaning Technologies</p>
+		</a>
+	</div>
+	<div id="footerLinks">
+		<a href="https://cheerpj.com/" target=”_blank”>cheerpj.com</a>
+	</div>
+</footer>
