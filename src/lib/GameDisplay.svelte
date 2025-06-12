59f7f2605ba41d9041db5ed7055f02b1f5a85a4e
Francesco <francesco@leaningtech.com>
2025-06-12 23:49:51 +0200

    added footer, navbar, split HTML into components

---
+++ src/lib/GameDisplay.svelte
@@ -0,0 +1,18 @@
+<h1>Browsercraft</h1>
+<div class="container">
+  <div class="intro">
+    <p>
+      This is a proof-of-concept demo of Minecraft 1.2.5 running unmodified in the browser.
+    </p>
+    <p>
+      Clicking the button below will download the client from mojang.com.
+      By clicking it, you agree to the <a href="https://www.minecraft.net/eula" target="_blank">Minecraft EULA</a>.
+    </p>
+    <button>Play!</button>
+    <div class="disclaimer">
+      This is not an official Minecraft product. It is not approved by or associated with Mojang or Microsoft.
+    </div>
+  </div>
+  <progress style="display: none"></progress>
+  <div class="display"></div>
+</div>
