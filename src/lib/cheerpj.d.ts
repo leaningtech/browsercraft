f94452f85f8532c6c7939b21101e884dc59f6a91
Francesco <francesco@leaningtech.com>
2025-06-17 11:43:33 +0200

    added timer to hide the the game after 3 minutes

--- src/lib/cheerpj.d.ts
+++ src/lib/cheerpj.d.ts
@@ -1,2 +1,6 @@
-declare const cheerpjInit: (options?: object) => Promise<void>;
-declare const cheerpjCreateDisplay: any;
+const cheerpjInit: (options?: object) => Promise<void>;
+const cheerpjCreateDisplay: any;
+const cheerpjRunMain: any;
+const cheerpOSOpen: any;
+const cheerpOSWrite: any;
+const cheerpOSClose: any;
