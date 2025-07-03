a49703b1fa7a3936ec19f7969edfb52d8e647133
Francesco <francesco@leaningtech.com>
2025-07-03 12:12:13 +0200

    added spinning wheel animation while CheerpJ is loading

--- src/lib/GameDisplay.svelte
+++ src/lib/GameDisplay.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { tryPlausible, showElement, hideElement, formatTime } from "./utilities";
+	import spinnerWhite from '$lib/assets/loading-spinner-white.svg';
 
 	const pathJarMinecraft = "/files/client_1.2.5.jar";
 	const urlDownloadMinecraft = "https://piston-data.mojang.com/v1/objects/4a2fac7504182a97dcbcd7560c6392d7c8139928/client.jar";
 	const pathJarLibs = `/app/lwjgl/lwjgl-2.9.3.jar:/app/lwjgl/lwjgl_util-2.9.3.jar:${pathJarMinecraft}`;
 
+	let loading: HTMLDivElement;
 	let display: HTMLDivElement;
 	let intro: HTMLDivElement;
 	let progressBar: HTMLProgressElement;
@@ -24,6 +26,9 @@
 			preloadResources:{"/lt/8/jre/lib/rt.jar":[0,131072,1310720,1572864,4456448,4849664,5111808,5505024,7995392,8126464,9699328,9830400,9961472,11534336,11665408,12189696,12320768,12582912,13238272,13369344,15073280,15335424,15466496,15597568,15990784,16121856,16252928,16384000,16777216,16908288,17039360,17563648,17694720,17825792,17956864,18087936,18219008,18612224,18743296,18874368,19005440,19136512,19398656,19791872,20054016,20709376,20840448,21757952,21889024,26869760],"/lt/etc/users":[0,131072],"/lt/etc/localtime":[],"/lt/8/jre/lib/cheerpj-awt.jar":[0,131072],"/lt/8/lib/ext/meta-index":[0,131072],"/lt/8/lib/ext":[],"/lt/8/lib/ext/index.list":[],"/lt/8/lib/ext/localedata.jar":[],"/lt/8/jre/lib/jsse.jar":[0,131072,786432,917504],"/lt/8/jre/lib/jce.jar":[0,131072],"/lt/8/jre/lib/charsets.jar":[0,131072,1703936,1835008],"/lt/8/jre/lib/resources.jar":[0,131072,917504,1179648],"/lt/8/jre/lib/javaws.jar":[0,131072,1441792,1703936],"/lt/8/lib/ext/sunjce_provider.jar":[],"/lt/8/lib/security/java.security":[0,131072],"/lt/8/jre/lib/meta-index":[0,131072],"/lt/8/jre/lib":[],"/lt/8/lib/accessibility.properties":[],"/lt/8/lib/fonts/LucidaSansRegular.ttf":[],"/lt/8/lib/currency.data":[0,131072],"/lt/8/lib/currency.properties":[],"/lt/libraries/libGLESv2.so.1":[0,262144],"/lt/libraries/libEGL.so.1":[0,262144],"/lt/8/lib/fonts/badfonts.txt":[],"/lt/8/lib/fonts":[],"/lt/etc/hosts":[],"/lt/etc/resolv.conf":[0,131072],"/lt/8/lib/fonts/fallback":[],"/lt/fc/fonts/fonts.conf":[0,131072],"/lt/fc/ttf":[],"/lt/fc/cache/e21edda6a7db77f35ca341e0c3cb2a22-le32d8.cache-7":[0,131072],"/lt/fc/ttf/LiberationSans-Regular.ttf":[0,131072,262144,393216],"/lt/8/lib/jaxp.properties":[],"/lt/etc/timezone":[],"/lt/8/lib/tzdb.dat":[0,131072]}
 		});
 		await cheerpjCreateDisplay(-1, -1, display);
+
+		hideElement(loading);
+		showElement(intro);
 	}
 
 	async function startGame() {
@@ -83,6 +88,7 @@
 	}
 
 	onMount(async () => {
+		loading = document.getElementById('loading');
 		display = document.getElementById('display');
 		intro = document.getElementById('intro');
 		progressBar = document.getElementById('progress-bar');
@@ -94,6 +100,10 @@
 </script>
 
 <div class="game-container">
+	<div id="loading" class="loading-container">
+		<img src={spinnerWhite} class="spinner" alt="Loading" />
+		<p class="text-center">Loading CheerpJ ...</p>
+	</div>
 	<div id="intro" class="intro">
 		<p>
 			This is a proof-of-concept demo of Minecraft 1.2.5 running unmodified in the browser.
