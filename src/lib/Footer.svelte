fd6ee3eb15bebd643fa6ff7b9d4a4e55d2214c22
Francesco <francesco@leaningtech.com>
2025-07-03 13:03:14 +0200

    added info paragraph on the side

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,15 +2,18 @@
 	import leanTechLogo from '$lib/assets/tower.svg'
 </script>
 
-<footer>
-	<hr class="solid">
-	<div id="footer-logo">
-		<img id = "lt-logo" src ={leanTechLogo} alt="Leaning Technologies logo"/>
-		<a href="https://leaningtech.com/" target=”_blank”>
-			<p id="footer-text">© 2023-2025 Leaning Technologies</p>
-		</a>
-	</div>
-	<div id="footer-links">
-		<a href="https://cheerpj.com/" target=”_blank”>cheerpj.com</a>
-	</div>
-</footer>
+<div class="footer-paragraph">
+	<p>
+		Interested in using CheerpJ for your own projects? Take a look at the <a href="https://cheerpj.com/docs/overview" target="_blank">documentation</a> and <a href="https://discord.gg/7xXW6NAdHT" target="_blank">join the Discord server</a> for questions and support.<br>
+	</p>
+	<p>
+		You can also sign-up for the first <a href="https://cheerpj-the-hackathon.devpost.com" target="_blank">CheerpJ Hackathon</a>, held between September and October 2025, with a £500 prize awaiting the winning team.
+	</p>
+</div>
+<hr class="solid">
+<div id="footer-logo">
+	<img id = "lt-logo" src ={leanTechLogo} alt="Leaning Technologies logo"/>
+	<a href="https://leaningtech.com/" target=”_blank”>
+		<p id="footer-text">© 2023-2025 Leaning Technologies</p>
+	</a>
+</div>
