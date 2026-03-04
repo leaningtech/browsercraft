fda75c46e16e46f0299d51e29d393fc9bd586221
Liamoscarmccaffrey <80630000+Liamoscarmccaffrey@users.noreply.github.com>
2026-03-04 10:22:03 +0000

    added browserpod ad as svelte, and added to page.svelte and commented out nav bar in page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
 	import '../app.css';
-	import Nav from 'labs/packages/global-navbar/src/Nav.svelte';
 	import GhLink from '$lib/GhLink.svelte';
 	import GameDisplay from '$lib/GameDisplay.svelte';
 	import Footer from '$lib/Footer.svelte';
 	import SideParagraph from '$lib/SideParagraph.svelte';
+	import BrowserPodAd from '$lib/BrowserPodAd.svelte';
 </script>
 
 <main>
-	<div class="nav-bar">
-		<Nav />
-	</div>
 	<div class="gh-container">
 		<GhLink />
 	</div>
 	<h1 class="title">BROWSERCRAFT</h1>
 	<div class="main-container">
 		<GameDisplay />
-		<SideParagraph />
+		<div class="side-column">
+			<BrowserPodAd />
+			<SideParagraph />
+		</div>
 	</div>
 	<div class="footer-container">
 		<Footer />
