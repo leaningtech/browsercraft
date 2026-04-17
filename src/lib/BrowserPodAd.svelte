197d61fe0425436cc178ae6056f23e09d02ec1e0
Alessandro Pignotti <a.pignotti@sssup.it>
2026-04-17 08:38:28 +0200

    Redirect BrowserPod promo to the new blog post

--- src/lib/BrowserPodAd.svelte
+++ src/lib/BrowserPodAd.svelte
@@ -1,4 +1,4 @@
-<a href="https://www.producthunt.com/products/browserpod-for-node-js" target="_blank" rel="noopener" class="bp-ad">
+<a href="https://labs.leaningtech.com/blog/browserpod-20" target="_blank" rel="noopener" class="bp-ad">
 	<div class="bp-ad-inner">
 		<div class="bp-ad-logo">
 			<svg class="bp-ad-svg" viewBox="-24 -24 714 714" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
@@ -23,8 +23,8 @@
 		</div>
 		<div class="bp-ad-text"> 
 			<div class="bp-ad-brand">BrowserPod 2.0</div>
-			<div class="bp-ad-tagline">In-browser code sandboxes - Launching today on Product Hunt.</div>
-			<div class="bp-ad-cta">Upvote &rarr;</div>
+			<div class="bp-ad-tagline">In-browser code sandboxes - run git, bash, node, python and much more!</div>
+			<div class="bp-ad-cta">Learn more &rarr;</div>
 		</div>
 		<div class="bp-ad-scanlines"></div>
 	</div>
